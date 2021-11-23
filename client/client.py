b1889fb960a5a58590433a36e3b2f26dd8aac33a
Cynthia Gao <jgao@iwu.edu>
2021-11-22 22:39:13 -0600

    Password and username could be saved in the server side

--- client/client.py
+++ client/client.py
@@ -4,37 +4,7 @@ import socket
 # https://stackoverflow.com/questions/35805078/how-do-i-convert-a-password-into-asterisks-while-it-is-being-entered
 # https://www.youtube.com/watch?v=27qfn3Gco00
 
-class filetransfer:
-  def __init__(self):
-    self.accounts = {}
-  def create_account(self):
-    checkaccount = input("Do you have an account? yes/no: ")
-    if checkaccount == "yes":
-      self.login()
-    elif checkaccount == "no":
-      while True:
-        username = input("Enter the username you want to register for this account: ")
-        password = input("Enter the password you want to register for this account: ")
-        if username not in self.accounts:
-          self.accounts[username] = password
-          self.login()
-          break
-        else:
-          print("The username you entered already existed, please change to another one")
-  def login(self):
-    while True:
-      user = input("Username: ")
-      pwd = input("Password: ")
-      if user in self.accounts and pwd == self.accounts[user]:
-        print('Logging on ...')
-        print('All done!')
-        break
-      else:
-        print('Incorrect password.')
-        print('Try again!')
 
-user1 = filetransfer()
-user1.create_account()
 
 
 s = socket.socket()
@@ -42,6 +12,35 @@ host = input(str("Please enter the host address of the sender: "))
 port = 8080
 s.connect((host, port))
 print("Connected Successfully")
+
+check_account = str(input("Do you have an account? yes/no: "))
+check_account_bytes = bytes(check_account, 'utf-8')
+s.send(check_account_bytes)
+
+have_account = s.recv(4096)
+have_account_str = have_account.decode('UTF-8')
+
+if have_account_str == "Not have account":
+    username = input("Enter the username you want to register for this account: ")
+    password = input("Enter the password you want to register for this account: ")
+    username_bytes = bytes(username, 'utf-8')
+    password_bytes = bytes(password, 'utf-8')
+    s.send(username_bytes)
+    s.send(password_bytes)
+    username = input("username: ")
+    password = input("password: ")
+    username_bytes = bytes(username, 'utf-8')
+    password_bytes = bytes(password, 'utf-8')
+    s.send(username_bytes)
+    s.send(password_bytes)
+if have_account_str == "Have account":
+    username = input("username: ")
+    password = input("password: ")
+    username_bytes = bytes(username, 'utf-8')
+    password_bytes = bytes(password, 'utf-8')
+    s.send(username_bytes)
+    s.send(password_bytes)
+
 
 file_name = input(str("Enter the desired filename: "))
 file = open(file_name, 'wb')
